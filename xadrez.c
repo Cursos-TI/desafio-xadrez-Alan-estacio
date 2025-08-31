d4faa7e5d641d9e9ef81bdd7f5364b84d3ec7dc7
Alan-estacio <alan.nunes15ti@gmail.com>
2025-08-31 16:57:20 +0000

    aventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,6 +2,7 @@
  
 int main() {
     int t;
+    int c;
     //torre se move 5 casas para direita
      printf("TORRE MOVE-SE PARA:\n");
     for ( t = 1; t <= 5; t++) {
@@ -18,10 +19,18 @@ int main() {
     printf("A RAINHA MOVE-SE:\n"); //a rainha se move 8 casas para esquerda
 int r = 0;
 do{
-printf("Esquerda\n", r);
+printf("ESQUERDA\n", r);
 r++;
 }
     while (r<8);
-     
+    printf("CAVALO MOVE-SE:\n");
+for ( c = 1; c <=2; c++) {
+    printf("ABAIXO\n", c);
+}
+  c = 0;
+    while (c<1){
+     printf("ESQUERDA\n", c);
+     c++;
+    }
     return 0;
 }
